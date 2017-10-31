a02eebb761926e878769ef2e0b1d4ee2ae08be3c
James N. V. Cash <james.nvc@gmail.com>
2017-10-31 14:12:51 -0400

    Add README and example

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # Kaleidoscope-ToIfAlone
+
+This plugin enables you to have a key that acts normally when tapped, but shifts to a layer when held in conjunction with pressing other keys.
+It is very similiar to [DualUse][], mainly differing by never timing out.
+
+It is made to emulate the behaviour of Karabiner's `ToIfAlone` functionality, hence the name.
+
+## Using the plugin
+
+Add the plugin to your available libraries by cloning it into your `Arduino-Boards/libraries` directory.
+
+```c++
+#include <Kaleidoscope.h>
+#include <Kaleidoscope-ToIfAlone.h>
+
+// Have an enum for layers
+// or some other way of assigning numbers to layers
+enum {
+  _BASE,
+  _NUMPAD,
+};
+
+void setup(void) {
+  Kaleidoscope.use(&ToIfAlone);
+
+  Kaleidoscope.setup();
+
+  static kaleidoscope::ToIfAlone::KeyBinding toifalonemap[] = {
+    {Key_Escape, _NUMPAD},
+    TOIFALONE_MAP_END
+  };
+  ToIfAlone.map = toifalonemap;
+}
+```
+
+## Plugin methods
+
+The plugin provides the `ToIfAlone` obkect, with the following property:
+
+### `.map`
+
+> Set the key map. This takes an array of `kaleidoscope::ToIfAlone::KeyBinding`
+> objects.
+> The last item in the array must be the special `TOIFALONE_MAP_END` value.
+> Each KeyBinding objects takes the key that will be pressed and the layer that it should shift to, respectively.
+>
+> By default, the map is empty.
+
+## Further reading
+
+Starting from the [example][] is the recommended way of getting started with the plugin.
+
+  [DualUse]: https://github.com/keyboardio/Kaleidoscope-DualUse
+  [example]: https://github.com/jamesnvc/Kaleidoscope-ToIfAlone/blob/master/examples/ToIfAlone/ToIfAlone.ino
---
+++ examples/ToIfAlone/ToIfAlone.ino
@@ -0,0 +1,80 @@
+/* -*- mode: c++ -*-
+ * Kaleidoscope-ToIfAlone -- Karabiner's ToIfAlone functionality for Kaleidoscope
+ * Copyright (C) 2017  James Cash
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <Kaleidoscope.h>
+#include <Kaleidoscope-ToIfAlone.h>
+
+// Have an enum for layers
+// or some other way of assigning numbers to layers
+enum {
+  _BASE,
+  _NUMPAD,
+};
+
+const Key keymaps[][ROWS][COLS] PROGMEM = {
+  [_BASE] = KEYMAP_STACKED
+  (
+    Key_NoKey,    Key_1, Key_2, Key_3, Key_4, Key_5, Key_NoKey,
+    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
+    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
+    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
+
+    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
+    Key_skip,
+
+    Key_skip,  Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_skip,
+    Key_Enter, Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,
+    Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
+    Key_skip,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,
+
+    Key_RightShift, Key_RightAlt, Key_Spacebar, Key_RightControl,
+    Key_skip),
+
+  [_NUMPAD] = KEYPMAP_STACKED
+  (
+___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___,
+   ___,
+
+   ___,  ___, Key_Keypad7, Key_Keypad8,   Key_Keypad9,        Key_KeypadSubtract, ___,
+   ___,                    ___, Key_Keypad4, Key_Keypad5,   Key_Keypad6,        Key_KeypadAdd,      ___,
+                           ___, Key_Keypad1, Key_Keypad2,   Key_Keypad3,        Key_Equals,         Key_Quote,
+   ___,                    ___, Key_Keypad0, Key_KeypadDot, Key_KeypadMultiply, Key_KeypadDivide,   Key_Enter,
+   ___, ___, ___, ___,
+   ___
+   ),
+};
+
+void setup(void) {
+  Kaleidoscope.use(&ToIfAlone);
+
+  Kaleidoscope.setup();
+
+  static kaleidoscope::ToIfAlone::KeyBinding toifalonemap[] = {
+    {Key_Escape, _NUMPAD},
+    TOIFALONE_MAP_END
+  };
+  ToIfAlone.map = toifalonemap;
+}
+
+void loop() {
+  Kaleidoscope.loop();
+}
